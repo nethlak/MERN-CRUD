db312a03685c01b6b99504c2efd8db17e6a62ea4
Devja <nethlakdevja@gmail.com>
2023-07-12 01:10:55 +0530

    till model

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extenden: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server started on port ${port}`));
